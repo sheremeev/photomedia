52240ccf38fc9adcca60b58649d19cba239a01c9
sheremeev <makssheremeev@gmail.com>
2022-12-15 21:28:50 +0400

    Add sticky navbar

--- src/App.vue
+++ src/App.vue
@@ -9,44 +9,45 @@
 </template>
 
 <script>
-  import Navigation from "@/components/Navigation";
-  import Footer from "@/components/Footer";
-  export default {
-    name: 'app',
-    computed: {
-      layout() {
-        console.log(this.$route.meta)
-        return (this.$route.meta.layout || 'main')
-      }
-    },
-    components: {
-      Footer,
-      Navigation
+import Navigation from "@/components/Navigation";
+import Footer from "@/components/Footer";
+
+export default {
+  name: 'app',
+  computed: {
+    layout() {
+      //console.log(this.$route.meta)
+      return (this.$route.meta.layout || 'main')
     }
+  },
+  components: {
+    Footer,
+    Navigation
   }
+}
 </script>
 
 
 <style lang="scss">
-  @import "css/bootstrap.min.css";
-  @import "css/owl.carousel.min.css";
-  @import "css/magnific-popup.css";
-  @import "css/font-awesome.min.css";
-  @import "css/themify-icons.css";
-  @import "css/nice-select.css";
-  @import "css/flaticon.css";
-  @import "css/animate.css";
-  @import "css/slicknav.css";
-  @import "css/style.css";
+@import "css/bootstrap.min.css";
+@import "css/owl.carousel.min.css";
+@import "css/magnific-popup.css";
+@import "css/font-awesome.min.css";
+@import "css/themify-icons.css";
+@import "css/nice-select.css";
+@import "css/flaticon.css";
+@import "css/animate.css";
+@import "css/slicknav.css";
+@import "css/style.css";
 
-  //@import "~bootstrap/dist/css/bootstrap.min.css";
-  //@import "~owl.carousel/dist/assets/owl.carousel.min.css";
-  //@import "~magnific-popup/dist/magnific-popup.css";
-  //@import "~font-awesome/css/font-awesome.min.css";
-  //@import "~@icon/themify-icons/themify-icons.css";
-  //@import "~jquery-nice-select/css/nice-select.css";
-  //@import "css/flaticon.css";
-  //@import "~animate.css";
-  //@import "css/slicknav.css";
-  //@import "css/style.css";
+//@import "~bootstrap/dist/css/bootstrap.min.css";
+//@import "~owl.carousel/dist/assets/owl.carousel.min.css";
+//@import "~magnific-popup/dist/magnific-popup.css";
+//@import "~font-awesome/css/font-awesome.min.css";
+//@import "~@icon/themify-icons/themify-icons.css";
+//@import "~jquery-nice-select/css/nice-select.css";
+//@import "css/flaticon.css";
+//@import "~animate.css";
+//@import "css/slicknav.css";
+//@import "css/style.css";
 </style>
